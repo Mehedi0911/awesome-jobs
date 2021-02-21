015a33049f888dff7a8738fa97501bd1eccb67b0
unknown <mmraaj378@gmail.com>
2021-02-21 19:17:02 +0600

    final

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ submitBtn.addEventListener('click', function(e){
     let jobId = Math.floor(Math.random() * 1000000);
     const jobTitle = document.getElementById("job-title");
     const jobSalary = document.getElementById("job-salary");
-    localStorage.setItem(jobTitle.value+"-"+jobId, jobTitle.value );
-    localStorage.setItem(jobTitle.value+"-salary-"+jobId, jobSalary.value );
-    const jobField = document.getElementById("jobs");
-    const newJob = document.createElement('div');
+    const refreshBtn = document.getElementById("refresh-btn");
+    // localStorage.setItem(jobTitle.value+"-"+jobId, jobTitle.value );
+    // localStorage.setItem(jobTitle.value+"-salary-"+jobId, jobSalary.value );
+    
     const newJobInfo = `<div class="new-job">
         <div class="single-job">
         <h6> Job ID : ${jobId}</h6>
@@ -20,15 +20,68 @@ submitBtn.addEventListener('click', function(e){
         </div>
         <button class="apply-btn">Apply</button>
     </div> `
-    newJob.innerHTML = newJobInfo;
-    jobField.appendChild(newJob);
     
+    
+   
     jobTitle.value = '';
     jobSalary.value = '';
-    
+
+
+
+if(localStorage.getItem('data') == null){
+    localStorage.setItem('data','[]')
+}
+
+var oldData = JSON.parse(localStorage.getItem('data'))
+oldData.push(newJobInfo);
+
+localStorage.setItem('data', JSON.stringify(oldData));
+
+
+refreshBtn.style.display = 'block';
     
 })
 
+const refreshBtn = document.getElementById("refresh-btn");
+refreshBtn.addEventListener('click', function(){
+    this.style.display = 'none';
+    const jobField = document.getElementById("jobs");
+    jobField.innerHTML = '';
+    jobData = JSON.parse(localStorage.getItem('data'));
+    jobData.forEach(job => {
+
+    console.log(job);
+
+    const newJob = document.createElement('div');
+    newJob.innerHTML = job;
+    jobField.appendChild(newJob);
+
+});
+})
+
+const jobField = document.getElementById("jobs");
+jobField.innerHTML = '';
+jobData = JSON.parse(localStorage.getItem('data'));
+jobData.forEach(job => {
+
+console.log(job);
+
+const newJob = document.createElement('div');
+newJob.innerHTML = job;
+jobField.appendChild(newJob);
+
+});
+
+
+
+
+
+
+
+// const age = [23, 23,43,63, 1];
+// localStorage.setItem("item",age);
+// const single = localStorage.getItem(Storage.item[0]);
+// console.log(single);
 
    
 
@@ -37,3 +90,5 @@ submitBtn.addEventListener('click', function(e){
 
 
 
+
+//     jobField.appendChild(newJob);
